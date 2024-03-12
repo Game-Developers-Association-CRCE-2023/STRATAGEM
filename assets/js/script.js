2c313b47a17934551d0773a65853516a4cc44b13
Mark Tuscano <tuscanomark.mt@gmail.com>
2024-03-12 15:55:19 +0530

    responsive and design changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,21 @@
-let menu = document.querySelector('#menu-icon');
-let navlist = document.querySelector('.navlist');
-
-// Function to toggle menu and navigation list
-function toggleMenu() {
-    menu.classList.toggle('bx-x');
-    navlist.classList.toggle('open');
-}
-
-// Event listener for menu icon click
-menu.onclick = () => {
-    toggleMenu();
-};
-
-// Event listener for window resize
-window.addEventListener('resize', () => {
-    // Check screen width and close the menu if it's a larger screen
-    if (window.innerWidth > 990) {
-        navlist.classList.remove('open');
-        menu.classList.remove('bx-x');
+window.addEventListener('scroll', function() {
+    const separator = document.querySelector('.seperator');
+    if (window.scrollY > 100) {
+        separator.style.display = 'none';
+    } else {
+        separator.style.display = 'block';
     }
 });
 
-// ScrollReveal initialization
 const sr = ScrollReveal({
     distance: '65px',
     duration: 2600,
     delay: 450,
+    easing: 'cubic-bezier(0.5, 0, 0, 1)',
     reset: true
 });
-
-// Reveal animations
 sr.reveal('.hero-text', { delay: 200, origin: 'top' });
 sr.reveal('.hero-img', { delay: 450, origin: 'top' });
-sr.reveal('.icons', { delay: 500, origin: 'left' });
 sr.reveal('.scroll-down', { delay: 500, origin: 'right' });
+sr.reveal('#about', { delay: 500, origin: 'top' });
+sr.reveal('#toggle-icon', { delay: 500, origin: 'left' });
